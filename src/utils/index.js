5a693752fa8daecf46c9f4db8a40f0e19ca258ef
Ренат Османов <renat.osmanov@Mac-mini-sysadmin.local>
2018-01-23 13:45:52 +0300

    stable sort util

--- src/utils/index.js
+++ src/utils/index.js
@@ -13,3 +13,27 @@ function predicateAndModifier(a, b) {
   return a.title === b.title && a.id === b.id && modifyObjs(a, b)
 }
 export const mergeItemsByIds = items => uniqWith(items, predicateAndModifier)
+
+function stableSort(arr, compare) {
+  const original = arr.slice(0)
+
+  arr.sort(function(a, b) {
+    const result = compare(a, b)
+    return result === 0 ? original.indexOf(a) - original.indexOf(b) : result
+  })
+
+  return arr
+}
+
+export const sort = (items, name, orderBy = 'ask') => {
+  if (orderBy === 'ask') {
+    return stableSort(
+      items,
+      (a, b) => (a[name] > b[name] ? 1 : a[name] < b[name] ? -1 : 0)
+    )
+  }
+  return stableSort(
+    items,
+    (a, b) => (a[name] < b[name] ? 1 : a[name] > b[name] ? -1 : 0)
+  )
+}
